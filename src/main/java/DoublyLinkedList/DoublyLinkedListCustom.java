e7beb62512d932399b84266ac1605f8b1f68e39f
TheresaBreitwieser <theresabreitwieser@gmail.com>
2022-05-04 08:05:00 +0200

    Finalized

--- src/main/java/DoublyLinkedList/DoublyLinkedListCustom.java
+++ src/main/java/DoublyLinkedList/DoublyLinkedListCustom.java
@@ -1,5 +1,8 @@
 package DoublyLinkedList;
 
+
+import java.util.Random;
+
 public class DoublyLinkedListCustom implements MyListInterface {
     private Node head;
     private Node tail;
@@ -13,6 +16,7 @@ public class DoublyLinkedListCustom implements MyListInterface {
             head= tail=newNode;
         } else {
             newNode.setNext(head);
+            head.setPrevious(newNode);
             head=newNode;
         }
         size++;
@@ -25,6 +29,7 @@ public class DoublyLinkedListCustom implements MyListInterface {
             head= tail=newNode;
         } else {
             newNode.setPrevious(tail);
+            tail.setNext(newNode);
             tail=newNode;
         }
         size++;
@@ -54,7 +59,9 @@ public class DoublyLinkedListCustom implements MyListInterface {
 
     @Override
     public void addElementAtRandomIndex(int value) {
-
+        Random random = new Random();
+        int randomIndex = random.nextInt(size);
+        addAtIndex(randomIndex, value);
     }
 
     @Override
@@ -82,6 +89,7 @@ public class DoublyLinkedListCustom implements MyListInterface {
         } else {
             temp = tail;
             tail = tail.getPrevious();
+            tail.setNext(null);
             size--;
         }
         return temp;
@@ -89,7 +97,19 @@ public class DoublyLinkedListCustom implements MyListInterface {
 
     @Override
     public Node removeAtIndex(int index) {
-        return null;
+        Node current;
+        if(isEmpty()) {
+            throw new IllegalArgumentException("List is empty");
+        } else {
+            current=head;
+            for(int i=0; i<index; i++) {
+                current=current.getNext();
+            }
+            current.getPrevious().setNext(current.getNext());
+            current.getNext().setPrevious(current.getPrevious());
+            size--;
+        }
+        return current;
     }
 
     @Override
@@ -104,50 +124,133 @@ public class DoublyLinkedListCustom implements MyListInterface {
 
     @Override
     public void printList() {
-
+        Node current = head;
+       while(current !=null) {
+           System.out.print(current.getValue() + ",");
+           current=current.getNext();
+       }if(isEmpty())
+       {
+            System.out.println("list is empty");
+        }
+        System.out.println();
     }
 
     @Override
     public void printListBackwards() {
-
+        Node current = tail;
+        while(current !=null){
+            System.out.print(current.getValue() + ",");
+            current=current.getPrevious();
+        }
+        System.out.println();
     }
 
     @Override
-    public int get(int index) throws IllegalArgumentException {
-        return-1;
+    public int get(int index) {
+        Node current=head;
+        if(isEmpty()){
+            throw new IllegalArgumentException("List is empty");
+        } else if(index>=size){
+            throw new IllegalArgumentException("Index is out of range");
+        } else {
+            for(int i=0; i<index; i++) {
+                current=current.getNext();
+            }
+        }
+        return current.getValue();
     }
 
-    //schwierig
+    //wir gehen von sortierter liste aus [0,1,1,2] - [0,1,2]
     @Override
     public void removeDuplicates() {
-//wir gehen von sortierter liste aus [0,1,1,2] - [0,1,2]
+        Node current=head;
+        if(isEmpty()) {
+            throw new IllegalArgumentException("list is empty");
+        } else {
+            for(int i=0; i<size-1; i++) {
+               while(current.getValue() == current.getNext().getValue()) {
+                   removeAtIndex(i+1);
+               }
+                current=current.getNext();
+            }
+        }
     }
 
-    //schwierig
+
     @Override
     public void reverseList() {
+        Node tempTail = tail;
+        tail=head;
+        head=tempTail;
+
+        Node current=head;
+
+        while(current !=null) {
+            Node temp2=current.getNext();
+            current.setNext(current.getPrevious());
+            current.setPrevious(temp2);
+            current=current.getNext();
+        }
 
     }
 
     @Override
     public DoublyLinkedListCustom copyList() {
-        return null; //liste 1:1 kopieren und duplikat liste wieder zurück geben
+        Node current=head;
+        DoublyLinkedListCustom copyListResult = new DoublyLinkedListCustom();
+        for(int i=0; i<size; i++) {
+            copyListResult.addLast(current.getValue());
+            current=current.getNext();
+        }
+        return copyListResult;
     }
 
     @Override
     public void clear() {
-        //head, tail, size, null
+       head = null;
+       tail=null;
+       size=0;
     }
 
-    //schwierig
     @Override
     public boolean insertAfter(int index, int data) {
-        return false;
+        if(index<0 || index>size){
+            throw new IllegalArgumentException();
+        } else if(index ==0) {
+            addFirst(data);
+        }else if(index==size) {
+            addLast(data);
+        } else {
+            Node newNode = new Node(data);
+            Node current = head;
+            for(int i=0; i<index; i++) {
+                current=current.getNext();
+            }
+            current = current.getNext();
+            newNode.setNext(current);
+            newNode.setPrevious(current.getPrevious());
+            current.getPrevious().setNext(newNode);
+            current.setPrevious(newNode);
+        }
+        size++;
+        return true;
     }
 
+    //[0,1,3,1,5,1]
+    //[0,3,1,5,1]
     @Override
-    public Node deleteKey(int index) {
-        return null; //gelöschte Node zurückgeben
+    public Node deleteKey(int value) {
+        Node current=head;
+        Node temp=null;
+        int count=1;
+        for(int i=0; i<size; i++){
+            if(current.getValue() == value && count==1) {
+                temp = removeAtIndex(i);
+                count++;
+            }
+            current=current.getNext();
+        }
+        return temp;
     }
 }
 
--- src/main/java/DoublyLinkedList/Main.java
+++ src/main/java/DoublyLinkedList/Main.java
@@ -3,8 +3,53 @@ package DoublyLinkedList;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedListCustom list = new DoublyLinkedListCustom();
-        //TODO test all methods
-        // don't forget to also check the head and tail references
+
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addLast(3);
+        list.addAtIndex(2,6);
+        list.printList();
+        list.clear();
+        list.printList();
+        list.addFirst(0);
+        list.addLast(1);
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(2);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.printList();
+        list.printListBackwards();
+//        list.removeAtIndex(2);
+        list.printList();
+        list.removeDuplicates();
+        list.printList();
+        list.insertAfter(2,6);
+        list.printList();
+//        DoublyLinkedListCustom resultDuplicate = new DoublyLinkedListCustom();
+//        resultDuplicate= list.copyList();
+//        resultDuplicate.printList();
+        list.reverseList();
+        list.printList();
+        list.addLast(6);
+        list.printList();
+        list.deleteKey(6);
+        list.printList();
+        System.out.println(list.get(0));
+//        list.addElementAtRandomIndex(10);
+//        list.addElementAtRandomIndex(10);
+        list.printList();
+        list.removeAtIndex(2);
+        list.removeFirst();
+        list.printList();
+//        list.removeLast();
+        list.printList();
+
+
+
+
+
 
     }
 }
